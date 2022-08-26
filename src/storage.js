680b76c5e378d1104beb97dab472bd7227809b1f
1lubo <1lubo@pm.me>
2022-08-26 13:56:50 +0200

    Add functions to check storage and to save, find, create objects

--- src/storage.js
+++ src/storage.js
@@ -1,3 +1,6 @@
+import { Project } from "./project";
+import { Task } from "./task";
+
 function storageAvailable(type) {
     let storage;
     try {
@@ -23,6 +26,49 @@ function storageAvailable(type) {
     }
 }
 
+function checkStorageAvailable() {
+    if (storageAvailable('localStorage')) {        
+        return 'Local Storage Available. Saved data will be available even after the browser is closed.';
+      }
+      else {        
+        return 'Local Storage Not Available. Saved Data will be deleted after the browser is closed.';
+      }
+}
+
+    function saveObject(object){        
+        window.localStorage.setItem(object.title, JSON.stringify(object));
+        console.log(`${object.type} with the name ${object.title} was successfully saved!`);
+    }
+
+    function createObject(string) {
+        var obj = JSON.parse(string);
+        var title = obj.title;
+        var type = obj.type;
+
+        if(type == 'project') {            
+            return new Project(title);
+        } else if (type == 'task') {
+            return new Task(title, obj.project, obj.dueDate, obj.priority, obj.completed);
+        }
+    }
+
+    function findObject(title, type){
+                
+        if (window.localStorage.getItem(title) == null){
+            return false;
+        }
+
+        var foundInStorage = window.localStorage.getItem(title);
+
+        if (foundInStorage.includes(title) && foundInStorage.includes(type)) {
+            return foundInStorage;
+        } else {
+            return false;
+        }
+        
+    }
+  
+
 export {
-    storageAvailable
+    storageAvailable, checkStorageAvailable, saveObject, findObject, createObject
 }
