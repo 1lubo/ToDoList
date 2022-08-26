65cb1c4efb1cf8a949475b7e6157372b97e09707
1lubo <1lubo@pm.me>
2022-08-26 15:22:15 +0200

    Add function to get project tasks from localstorage

--- src/project.js
+++ src/project.js
@@ -2,6 +2,20 @@
 function Project (title) {
     this.type = 'project';
     this.title = title;
+    this.tasks = [];
+}
+
+Project.prototype.getTasks = function () {
+    var title = this.title;
+    var project = this;
+    Object.keys(localStorage).forEach(function(key){
+        if(localStorage.getItem(key).includes(`"type":"task"`) && 
+        localStorage.getItem(key).includes(`"project":"${project.title}"`)
+        ) {
+            project.tasks.push(localStorage.getItem(key));
+        }
+
+     });
 }
 
 export {Project}
