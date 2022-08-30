64402c23538b583a29cf5bf80fee7768c32ef5d6
1lubo <1lubo@pm.me>
2022-08-30 16:25:31 +0200

    remove unused imports

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import { createNavBar } from "./navbar";
-import { storageAvailable } from "./storage";
+import { saveObject } from "./storage";
+import { Project } from "./project";
+import { Task } from "./task";
+import { buildTask } from "./displayTask";
+import { buildProject } from "./displayProject";
 import './style.css';
 
-document.body.appendChild(createNavBar());
 
-if (storageAvailable('localStorage')) {
-    // Yippee! We can use localStorage awesomeness
-    console.log('Local Storage Available');
-  }
-  else {
-    // Too bad, no localStorage for us
-    console.log('Local Storage Not Available ');
-  }
-  
+//window.addEventListener('onload', alert(checkStorageAvailable()));
+createNavBar();
 
-//document.body.appendChild(addElement(['div', 'Hello World']));
+var proj = new Project('default');
+
+saveObject(proj);
+
+
+
+for (let i = 1; i < 11; i++){    
+    var task = new Task(`task${i}`, 'default', addDays(new Date(),Math.floor(Math.random() * 10)), Math.floor(Math.random() * (4 - 1) + 1));
+    saveObject(task);
+}
+
+
+
+//var loadTask = findObject('default', 'project');
+
+
+var content = document.createElement('div');
+content.classList.add('content');
+//content.appendChild(buildProject(loadTask));
+Object.keys(localStorage).forEach(function(key){
+    if(localStorage.getItem(key).includes(`"type":"task"`)){
+        content.appendChild(buildTask(localStorage.getItem(key)))
+    }
+})
+
+
+document.body.appendChild(content);
+
+
