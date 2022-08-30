d1c5e364eca290d05c4a77116b6c299ed9d39aed
1lubo <1lubo@pm.me>
2022-08-30 16:27:40 +0200

    add icons, add separate project section

--- src/navbar.js
+++ src/navbar.js
@@ -1,21 +1,38 @@
 import { addElement } from './buildingblocks';
 
-function createNavLink(text){
-    const navLink = addElement('div', text);
-    navLink.classList.add('navbarlink');
+function createNavLink(text, icon){
+    let navLinkContainer = addElement('div');
+    let navIcon = addElement('span', icon);
+    let navLink = addElement('div', text);    
     navLink.id = text;
-    return navLink;
+    navLinkContainer.appendChild(navLink);
+    navLinkContainer.appendChild(navIcon);
+    navLinkContainer.classList.add('navbarlink');
+    return navLinkContainer;
 }
 
-function createNavBar(){
-    //const header = addElement(['header']);
-    const navBarContainer = addElement('div');
-    navBarContainer.classList.add('navbar');
-    var navbarLinks = ['Home', 'Today', 'Upcoming', 'Projects', 'Add Project'];
-    
-    navbarLinks.forEach( link => navBarContainer.appendChild(createNavLink(link)));    
+function createProjectsSection(){
+    let projectsContainer = addElement('div');
+    projectsContainer.classList.add('projects-container');
+    let projectsHeader = addElement('div');
+    projectsHeader.classList.add('projects-header');
+    projectsHeader.appendChild(addElement('h4', 'Projects'));
+    projectsHeader.appendChild(addElement('span', `\u{2295}`));
+    projectsContainer.appendChild(projectsHeader);
+    projectsContainer.appendChild(addElement('div')).classList.add('project-list');
 
+    return projectsContainer;
+}
+
+function createNavBar(){    
+    let navBarContainer = addElement('div');
+    navBarContainer.classList.add('navbar');
+    let navbarLinks = [['Inbox', `\u{1F4E5}`], ['Today', `\u{2605}`], ['Upcoming', `\u{1F4C5}`]];
+    
+    navbarLinks.forEach( link => navBarContainer.appendChild(createNavLink(link[0], link[1])));    
+    navBarContainer.appendChild(createProjectsSection());
     document.body.appendChild(navBarContainer);
+    
 }
 
 export {
