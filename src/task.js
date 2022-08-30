a8e40c34160c84f62f8dd8fd2e353d573e8a1599
1lubo <1lubo@pm.me>
2022-08-30 11:55:36 +0200

    add task description value

--- src/task.js
+++ src/task.js
@@ -1,11 +1,13 @@
 
-function Task (title, project= null, dueDate = null, priority = 1, completed = false) {
+function Task (title, project= null, dueDate = null, priority = 4, completed = false, description = null,) {
     this.type = 'task';
     this.project = project;
     this.title = title;
     this.dueDate = dueDate;
     this.priority = priority;
     this.completed = completed;
+    this.description = description;
+
 }
 
 export {Task}
