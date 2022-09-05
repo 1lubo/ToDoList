ff1b83fe62bd42648bbbc568af9409b31a094f69
1lubo <1lubo@pm.me>
2022-09-05 09:58:00 +0200

    add modal functionality

---
+++ src/modal.js
@@ -0,0 +1,44 @@
+import { addElement } from "./buildingblocks";
+import { newTaskForm, hideNewTaskFormButton, closeExpandedTaskButton } from "./formsAndButtons";
+import { allTasks } from "./storage";
+import { buildExpandedTask } from "./displayTask"
+
+function newTaskModal(){
+    let modal = addElement('div');
+    modal.classList.add('modal');
+    modal.id = 'myModal';
+    let modalContent = addElement('div');
+    modalContent.classList.add('modal-content');
+    modalContent.appendChild(newTaskForm());
+    modal.appendChild(modalContent);
+    
+    return modal;
+}
+
+function showTaskModal(taskTitle){
+    let modal = addElement('div');
+    modal.classList.add('modal');
+    modal.id = 'myModal';
+    let modalContent = addElement('div');
+    modalContent.classList.add('modal-content');    
+    let task = allTasks().find(e => e.title = taskTitle);
+    buildExpandedTask(task).forEach( e => modalContent.appendChild(e));    
+    let submitFormButton = addElement('button', 'Save');
+    submitFormButton.id = 'close-expanded-task';
+    modalContent.appendChild(submitFormButton);
+    let cancelFormButton = addElement('button', 'Cancel');    
+    cancelFormButton.id = 'cancel-task-form';
+    modalContent.appendChild(cancelFormButton);
+
+    modal.appendChild(modalContent);
+    
+    let root = document.getElementsByClassName('project-tasks')[0];
+
+    root.appendChild(modal);
+    hideNewTaskFormButton();
+    closeExpandedTaskButton(taskTitle); 
+}
+
+export {
+    newTaskModal, showTaskModal
+}
