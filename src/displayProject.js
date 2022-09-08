969e7426f67921405276878e1a25603795b883da
1lubo <1lubo@pm.me>
2022-09-08 17:12:55 +0200

    add filter functionality

--- src/displayProject.js
+++ src/displayProject.js
@@ -1,13 +1,32 @@
 import { addElement } from "./buildingblocks";
 import { findObject, createObject } from "./storage";
 import { buildTask } from "./displayTask";
-import { deleteProjectButton } from "./formsAndButtons";
+import { deleteProjectButton, projectFilterButton } from "./formsAndButtons";
 
 
-function buildProject(projectString) {
+function buildProject(projectString, filterName=null) {
     
     let project = createObject(projectString);
-    project.getTasks();    
+    project.getTasks();  
+    
+    switch (filterName) {
+        case 'Alphabetically':
+            
+            project.tasks.sort((taskA, taskB)=> taskA.title.localeCompare(taskB.title))
+            break;
+        case 'Due Date':
+            
+            project.tasks.sort((taskA, taskB)=> new Date(taskA.dueDate) - new Date(taskB.dueDate))
+            break;
+        case 'Priority':
+            
+            project.tasks.sort((taskA, taskB)=> taskA.priority - taskB.priority)
+            break;
+        default:
+            
+            break
+    }
+    
     let projectContainer = addElement('div');
     projectContainer.classList.add('project-container');
     let projectTitle = addElement('h1', project.title);
@@ -19,6 +38,7 @@ function buildProject(projectString) {
     deleteProject.id = 'delete-project';
 
     projectContainer.appendChild(projectTitle);
+    projectContainer.appendChild(buildTaskfilterDropdown());
     
     if(project.title != 'Inbox'){
         projectContainer.appendChild(deleteProject);
@@ -42,14 +62,32 @@ function removeProjectFromNavbar(projectTitle) {
     document.querySelector('.projects-list').children[projectTitle].remove()
 }
 
-function showProject(projectTitle){
+function showProject(projectTitle, filterName=null){
     
     document.body.appendChild(addElement('div')).classList.add('content');
     const root = document.getElementsByClassName('content')[0];
-    root.appendChild(buildProject(findObject(projectTitle, 'project')));
+    root.appendChild(buildProject(findObject(projectTitle, 'project'), filterName));
     if(projectTitle != 'Inbox'){        
         deleteProjectButton()
     }
+    projectFilterButton()
+}
+
+function buildTaskfilterDropdown() {
+    let dropDown = addElement('div');
+    dropDown.classList.add('filter-dropdown');
+    let dropDownButton = addElement('div', `\u{1F39B}`);
+    dropDownButton.classList.add('dropbtn-filter');
+    let dropDownContent = addElement('div');
+    dropDownContent.classList.add('filter-dropdown-content');
+    ['Alphabetically', 'Due Date', 'Priority'].forEach( filter => {        
+        dropDownContent.appendChild(addElement('div', filter));
+    })
+
+    dropDown.appendChild(dropDownButton);
+    dropDown.appendChild(dropDownContent);
+
+    return dropDown
 }
 
 
