e925eb45149b7c793857f0af1320351b93b6cce0
1lubo <1lubo@pm.me>
2022-09-14 10:58:46 +0200

    fix date issue

--- src/task.js
+++ src/task.js
@@ -1,11 +1,11 @@
 import { saveObject } from "./storage";
 import { projectTaskNames } from "./storage";
 
-function Task (title, project= 'Inbox', dueDate = null, priority = 4, completed = false, description = null,) {
+function Task (title, project= 'Inbox', dueDate, priority = 4, completed = false, description = null) {
     this.type = 'task';
     this.project = project;
     this.title = title;
-    this.dueDate ||= new Date();
+    this.dueDate = dueDate || new Date();
     this.priority = priority;
     this.completed = completed;
     this.description = description;
@@ -23,11 +23,10 @@ function validateTaskName(taskTitle, projectTitle){
     return true
 }
 
-function createTask(title, project, dueDate, priority, completed, description) {
+function createTask(title, project, dueDate, priority, completed, description) {    
     
     let task = new Task(title, project, dueDate, priority, completed, description);
-    saveObject(task);
-
+    saveObject(task);      
     return task;
 }
 
